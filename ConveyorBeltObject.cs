6f85ff297878f6956ac144cdc404bb200b2e26b5
alex dana <alex.dana@epitech.eu>
2020-06-18 21:03:54 +0200

    first refactor

--- ConveyorBeltObject.cs
+++ ConveyorBeltObject.cs
@@ -35,6 +35,9 @@ namespace Eco.Mods.TechTree
     private Orientation _orientation = Orientation.NORTH;
     private List<Inventory> myInventories = new List<Inventory>();
 
+    private PublicStorageComponent front = null;
+    private PublicStorageComponent back = null;
+
     private bool done = false;
 
     private enum Orientation
@@ -49,8 +52,6 @@ namespace Eco.Mods.TechTree
     {
       base.OnCreate(creator);
       StockpileComponent.ClearPlacementArea(creator, this.Position3i, DefaultDim, this.Rotation);
-      // ChatManager.ServerMessageToAll(Localizer.Format("ROT x:{0} y:{1} z:{2}", this.Rotation.Right.x, this.Rotation.Right.y, this.Rotation.Right.z), true);
-      // ChatManager.ServerMessageToAll(Localizer.Format("POS x:{0} y:{1} z:{2}", this.Position3i.x, this.Position3i.y, this.Position3i.z), true);
     }
 
     protected override void Initialize()
@@ -69,7 +70,6 @@ namespace Eco.Mods.TechTree
 
 
       this.GetComponent<StockpileComponent>().Initialize(DefaultDim);
-      // ChatManager.ServerMessageToAll(Localizer.Format("myInventories {0}", myInventories), false);
 
       PublicStorageComponent storage = this.GetComponent<PublicStorageComponent>();
       storage.Initialize(DefaultDim.x * DefaultDim.z);
@@ -79,16 +79,18 @@ namespace Eco.Mods.TechTree
 
     public override void Tick()
     {
-      if (!done)
-      {
-        this.initialize();
-      }
       if (updateThrottle.DoUpdate)
       {
         if (this.isConveyorEmpty())
+        {
+          if (back == null) this.initialize(true);
           PullFromBack();
+        }
         else
+        {
+          if (front == null) this.initialize(false);
           PushFront();
+        }
       }
     }
 
@@ -114,107 +116,7 @@ namespace Eco.Mods.TechTree
       return base.OnActInteract(context);
     }
 
-    private void initialize()
-    {
-      IEnumerable<WorldObject> list = WorldObjectManager.GetObjectsWithin(this.Position3i, 5f);
-      foreach (WorldObject item in list)
-      {
-        ChatManager.ServerMessageToAll(Localizer.Format("INIT {0}", item), true);
-      }
-      done = true;
-    }
-
-    private void PushFront()
-    {
-      WorldObjectBlock blockInFront = GetNextBlock(false);
-
-      PublicStorageComponent our = this.GetComponent<PublicStorageComponent>();
-
-      if (blockInFront != null && !blockInFront.Is<Empty>())
-      {
-        // we have another conveyorbelt in front of us (or a not-filled stockpile)
-        var o = blockInFront.WorldObjectHandle.Object;
-        PublicStorageComponent front = o.GetComponent<PublicStorageComponent>();
-        MoveFromTo(our, front);
-      }
-      else
-      {
-        // if front is not a worldObjectBlock, try to move self into linked stockpiles
-        var invToPushIn = myInventories != null ? myInventories.FirstOrDefault() : null;
-        if (invToPushIn != null)
-        {
-          Inventory ourStorage = our.Storage;
-          if (ourStorage == null) return;
-          IEnumerable<ItemStack> stacks = ourStorage.Stacks;
-          ItemStack stack = stacks.FirstOrDefault();
-          if (stack != null)
-          {
-            Item itemToGive = stack.Item;
-            if (itemToGive != null && invToPushIn != null)
-            {
-              int itemQuantity = DefaultDim.y; //stack.Quantity;
-              ourStorage.TryMoveItems<Item>(itemToGive.Type, itemQuantity, invToPushIn);
-            }
-          }
-        }
-        else
-        {
-          // ChatManager.ServerMessageToAll(Localizer.Format("Object in front is not storage"), false);
-        }
-
-      }
-
-    }
-
-    private void PullFromBack()
-    {
-      WorldObjectBlock blockInBack = GetNextBlock(true);
-
-      PublicStorageComponent our = this.GetComponent<PublicStorageComponent>();
-      if (blockInBack != null && !blockInBack.Is<Empty>())
-      {
-        var worldObject = blockInBack.WorldObjectHandle.Object;
-
-        // Skip if it's a conveyor
-        if (worldObject.DisplayName == DisplayName) return;
-
-        PublicStorageComponent back = worldObject.GetComponent<PublicStorageComponent>();
-        MoveFromTo(back, our);
-      }
-      else
-      {
-        var invToPullFrom = GetFirstNotEmptyInventory(myInventories);
-        if (invToPullFrom != null && our != null)
-        {
-          MoveFromTo(invToPullFrom, our.Storage);
-        }
-      }
-    }
-
-    private Inventory GetFirstNotEmptyInventory(IEnumerable<Inventory> inventories)
-    {
-      if (myInventories == null) return null;
-      foreach (Inventory inv in inventories)
-      {
-        if (!inv.IsEmpty) return inv;
-      }
-      return null;
-    }
-    private ItemStack GetFirstItemStackNotEmpty(IEnumerable<ItemStack> stacks)
-    {
-      foreach (ItemStack stack in stacks)
-      {
-        if (!stack.Empty) return stack;
-      }
-      return null;
-    }
-
-    private bool isAStorageComponent(Block block)
-    {
-      return (!block.Is<Empty>() && block.GetType() == typeof(WorldObjectBlock) && ((WorldObjectBlock)block).WorldObjectHandle.Object.GetComponent<PublicStorageComponent>() != null);
-    }
-
-    private WorldObjectBlock GetNextBlock(bool inverted)
+    private void initialize(bool inverted)
     {
       Vector3i wantedPosition;
       if ((!inverted && _orientation == Orientation.NORTH) || (inverted && _orientation == Orientation.SOUTH))
@@ -229,22 +131,47 @@ namespace Eco.Mods.TechTree
         wantedPosition = new Vector3i(this.Position3i.x, this.Position3i.y, this.Position3i.z);
 
       // check front then front-top then front-bottom
-      Block block = World.GetBlock(wantedPosition);
-      Block blockTop = World.GetBlock(wantedPosition + new Vector3i(0, 1, 0));
-      Block blockBottom = World.GetBlock(wantedPosition + new Vector3i(0, -1, 0));
-      if (isAStorageComponent(block))
+      Vector3i blockTop = wantedPosition + new Vector3i(0, 1, 0);
+      Vector3i blockBottom = wantedPosition + new Vector3i(0, -1, 0);
+
+      IEnumerable<WorldObject> list = WorldObjectManager.GetObjectsWithin(this.Position3i, 5f);
+      foreach (WorldObject item in list)
       {
-        return (WorldObjectBlock)block;
+        List<Vector3i> occupancy = item.WorldOccupancy;
+        foreach (Vector3i position in occupancy)
+        {
+          if (inverted == false)
+          {
+            if (front == null && (position == wantedPosition || position == blockTop || position == blockBottom))
+              front = item.GetComponent<PublicStorageComponent>();
+          }
+          else
+          {
+            if (back == null && (position == wantedPosition || position == blockTop || position == blockBottom))
+              back = item.GetComponent<PublicStorageComponent>();
+          }
+        }
       }
-      else if (isAStorageComponent(blockTop))
+    }
+
+    private void PushFront()
+    {
+      PublicStorageComponent our = this.GetComponent<PublicStorageComponent>();
+      if (front != null) MoveFromTo(our, front);
+    }
+    private void PullFromBack()
+    {
+      PublicStorageComponent our = this.GetComponent<PublicStorageComponent>();
+      if (back != null) MoveFromTo(back, our);
+    }
+
+    private ItemStack GetFirstItemStackNotEmpty(IEnumerable<ItemStack> stacks)
+    {
+      foreach (ItemStack stack in stacks)
       {
-        return (WorldObjectBlock)blockTop;
+        if (!stack.Empty) return stack;
       }
-      else if (isAStorageComponent(blockBottom))
-      {
-        return (WorldObjectBlock)blockBottom;
-      }
-      else return null;
+      return null;
     }
 
     private void MoveFromTo(PublicStorageComponent from, PublicStorageComponent to)
